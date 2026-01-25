e2de3e650e4b1cd8e86b0f9186b3f9e2b666acf6
Naveen23190 <kumarnaven590@gmail.com>
2026-01-26 01:31:47 +0530

    v1.0.02

--- Script.js
+++ Script.js
@@ -8,3 +8,8 @@ function hideSidebar()
     const hideSidebar = document.querySelector(".Side-bar")
     hideSidebar.style.display = "none";
 }
+function reload()
+{
+    window.location.reload();
+    alert("Your Page Has Been Reloaded.....");
+}
