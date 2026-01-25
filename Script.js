bfb16bc3f481946194b10775a09cfa55cef6c4e9
Naveen23190 <kumarnaven590@gmail.com>
2026-01-25 23:20:38 +0530

    v1.0.01

---
+++ Script.js
@@ -0,0 +1,10 @@
+function showSidebar()
+{
+    const sidebar = document.querySelector(".Side-bar");
+    sidebar.style.display = "flex";
+}
+function hideSidebar()
+{
+    const hideSidebar = document.querySelector(".Side-bar")
+    hideSidebar.style.display = "none";
+}
